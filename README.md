3c24033cea86fc9bdec67659e9dcd67be7dec82b
Talon Daniels <talondaniels@gmail.com>
2016-07-05 17:33:15 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# project
+
+## License
+
+ - **MIT** : http://opensource.org/licenses/MIT
---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+export default function hello(name) {
+  return `hello, ${name}`;
+}
+
+function hello2(name) {
+  return `hello, ${name} ${name}`;
+}
